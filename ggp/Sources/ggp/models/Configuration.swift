19c22e60f3c0835ae968d27646437e8f6403aad2
Walter <walterjgsp@gmail.com>
2019-11-06 10:24:45 -0300

    Parsing json file to object working

--- ggp/Sources/ggp/models/Configuration.swift
+++ ggp/Sources/ggp/models/Configuration.swift
@@ -1,5 +1,28 @@
-class Configuration{
+import Foundation
+
+class Configuration : Decodable {
     var crossover_rate: Double = 0.0
     var mutation_rate: Double = 0.0
     var population: Int = 0
+
+    static let shared = Configuration()
+
+    init(){}
+
+    private enum CodingKeys: String, CodingKey {
+        case crossover_rate
+        case mutation_rate
+        case population
+    }
+
+    func decode(data: Data) throws -> Configuration? {
+        do {
+            let decoder = JSONDecoder()
+            let configuration = try decoder.decode(Configuration.self, from: data)
+            return configuration
+        } catch let error {
+            LoggerHelper.shared.logError(message: error.localizedDescription)
+            return nil
+        }
+    }
 }
--- ggp/Sources/ggp/repository/ConfigurationRepository.swift
+++ ggp/Sources/ggp/repository/ConfigurationRepository.swift
@@ -6,35 +6,34 @@ class ConfigurationRepository{
 
     init(){}
     
-    func openConfigurationFile() -> Configuration{
+    func openConfigurationFile() -> Configuration? {
 
         let fileName = "config"
         let path = FileManager.default.currentDirectoryPath+"/Assets"
         let dir = try? URL.init(fileURLWithPath: path)
 
         if let fileURL = dir?.appendingPathComponent(fileName).appendingPathExtension("json") {
-            // Then reading it back from the file
-            var inString = ""
             do {
-                inString = try String(contentsOf: fileURL)
+                let content = try Data(contentsOf: fileURL)
+                let configuration = try Configuration.shared.decode(data: content)
+                print(configuration!.crossover_rate)
             } catch {
-                LoggerHelper.shared.logError(message:"Failed reading from URL: \(fileURL), Error: \(error.localizedDescription)"
-                )
+                LoggerHelper.shared.logError(message:"Failed reading from URL: \(fileURL), Error: \(error.localizedDescription)")
             }
 
-            do {
-                print(inString)
-                let data = inString.data(using: .utf8)!
-                if let jsonArray = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [Dictionary<String,Any>]{
-                print(jsonArray) // use the json here     
-                } else {
-                    LoggerHelper.shared.logError(message:"Error when parsing string to json")
-                }
-            } catch let error as NSError {
-                LoggerHelper.shared.logError(message:error.localizedDescription)
-            }
+            // do {
+            //     print(inString)
+            //     let data = inString.data(using: .utf8)!
+            //     if let jsonArray = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [Dictionary<String,Any>]{
+            //     print(jsonArray) // use the json here     
+            //     } else {
+            //         LoggerHelper.shared.logError(message:"Error when parsing string to json")
+            //     }
+            // } catch let error as NSError {
+            //     LoggerHelper.shared.logError(message:error.localizedDescription)
+            // }
         }
         
-        return Configuration()
+        return nil
     }
 }
