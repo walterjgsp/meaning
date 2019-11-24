f831a327720c180ab95c3b282b66c72e52e7f7e6
Walter José <walterjgsp@gmail.com>
2019-11-24 17:32:39 -0300

    Adding Travis CI

--- sk_processor/src/tests/test_generators.py
+++ sk_processor/src/tests/test_generators.py
@@ -3,7 +3,12 @@ from core.generators import *
 from models.algorithm import Algorithm
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 def test_generator_DecisionTreeClassifier():
     algorithm = Algorithm("from sklearn.tree import DecisionTreeClassifier","DecisionTreeClassifier",[])
     assert isinstance(generateModel(algorithm),DecisionTreeClassifier), "test failed"
+
+def test_generator_KNeighborsClassifier():
+    algorithm = Algorithm("from sklearn.neighbors import KNeighborsClassifier","KNeighborsClassifier",[])
+    assert isinstance(generateModel(algorithm),KNeighborsClassifier), "test failed"
